19209f629a0f6986ad53a0bcb41316e3a582b947
Rodolpho <nikersonrodolpho@gmail.com>
2022-08-17 22:15:17 -0300

    Exercícios da Aula 16

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let number = +prompt("Digite um número.");
+
+let oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(num in oneToTen) {
+    console.log(number * oneToTen[num])
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+const animals = ["Lontra", "Capivara", "Porquinho da Índia", "Cão de Pradaria", "Furão"]
+
+for(animal of animals) {
+    console.log(+animals.indexOf(animal)+1, "-", animal)
+}
