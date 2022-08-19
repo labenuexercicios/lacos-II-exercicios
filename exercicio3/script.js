d15f67a41a166b25f1882ed4e519c1caf2e7a3c1
falcaomm <manu.m.falcao@gmail.com>
2022-08-18 21:05:43 -0300

    ex fixacao feedback

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,25 +1,15 @@
 const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
 
-for (let i of maioresPaises) {
-    let string = `${i} - `;
-    for (let j in maioresPaises) {
-        string += (`${Number(j)}`);
-    }
-    console.log(string);
-
-}
-
-
 for (let i in maioresPaises) {
     let string = (`${Number(i)+1} - `);
-    for (let j of maioresPaises) {
+    for (let j of maioresPaises[i]) {
        string +=(`${j}`);
     }
     console.log(string);
 }
 
+let i = 1
 for (let j of maioresPaises) {
-    let i = 0
+    console.log(`${i} - ${j}`);
     i++
-    console.log(`${i} ${j}`);
 }
