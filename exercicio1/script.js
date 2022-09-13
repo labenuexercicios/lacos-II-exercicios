2106a5cd8dfa21787bb1184e90816db72d315b2f
PYTERODACTYL <pyter512@live.com>
2022-09-13 14:19:11 -0300

    feitoo

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,10 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+let jogadorNumero=1
+for (let i=0;i<array.length;i++){
+    for(let j=0;j<array[i].length;j++){
+        console.log("Jogador "+jogadorNumero+": "+array[i]);
+    }
+    jogadorNumero++;
+}
---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+let numero = prompt("Insira um numero")
+let tabuada = parseInt(numero)
+let multiplica = parseInt(numero)
+const tabuadaArray=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (i in tabuadaArray) {
+    console.log(tabuada);
+    tabuada = tabuada + multiplica
+}
