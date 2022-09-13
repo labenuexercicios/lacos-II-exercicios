241ec1bbb2a25a8c58b0c85920ffb2e254383d31
Byenktrovik <byenktrovik@gmail.com>
2022-09-13 17:52:23 -0300

    exercicio

---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+
+
+
+const tabuada = +prompt("Insira um número")
+
+let numero = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+
+for(let i in numero){
+    console.log(`${tabuada} x ${numero[i]} = ${tabuada*numero[i]}`)
+}
+
+
+
+
+
+
+
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,18 @@
+
+
+//Criar array com 5 strings
+
+
+const badOmens = ["Sepphie", "Meow", "Debbie", "Aquiles", "Misao"]
+let contador = 1
+
+for (i of badOmens) {
+    console.log(`${contador}-${i}`);
+    contador ++
+  }
+
+
+
+
+
+
