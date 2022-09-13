87ad16bb20bd63e3c1d209bee8380f6c906040d8
italodve <italovinicius@Gmail.com>
2022-09-13 15:44:34 -0300

    feito

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+const tabuada = [1,2,3,4,5,6,7,8,9,10]
+const numero = Number(prompt("Informe um número  de 1 a 10"))
+
+for(const i in tabuada){
+console.log(`${numero} x ${tabuada[i]} :7`, tabuada[i] * numero)
+}
+
+    
---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+let k = 0
+for(let j of maioresPaises){
+    k += 1
+    console.log( ` ${k} - ${j}` )
+
+}
