67bca3c0011a769a5a5b0ab737d02196df26886d
GabrielOlivaa <gedfto@gmail.com>
2022-09-13 15:58:44 -0300

    exercicos ok

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,18 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+
+// for (i in array){
+//     console.log(`jogador ${Number(i)+1}: ${array[i]}`)
+// }
+
+// JEITO PARA USUAR COM OUTRAS LINGUAGENS ABAIXO. 
+
+for (let i in array){
+let temporada = `temporada ${ Number(i)+1}:`
+    for (let j of array[i]){
+        temporada +=   j  +  ", "
+    }
+    console.log(temporada)
+}
