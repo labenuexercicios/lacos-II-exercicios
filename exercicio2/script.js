1ba97b198160ac7b29e38d817fe614e6d86edcf2
Vanessa Farias <vr.farias1992@gmail.com>
2022-09-13 22:23:23 -0300

    laços II em JS

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+let tabuada = [1,2,3,4,5,6,7,8,9,10]
+
+let numero = Number(prompt("Digite o seu numero"));
+
+
+for (element in tabuada){
+    console.log(tabuada[element]*numero);
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+let lista = 1;
+
+for (i of maioresPaises){
+    console.log(`${lista} -  ${i}`);
+    lista++
+}
