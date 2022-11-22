1b29a75a70b74274708dd9eb0ab9edd99849dabf
Lucas Breia <lucas@MacBook-Air-de-Lucas.local>
2022-11-22 00:26:50 -0300

    Exercícios de Treinamento

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,14 @@
-const array = [
+//EXERCÍCIO 1
+const arrayGolsJogador = [
     [27, 4, 20, 13, 14],
     [11, 15, 12, 8, 9],
     [5, 5, 12, 16, 4],
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < array[i].length; j++){
+        console.log(`Jogador ${i + 1}: ${array[i]}`)
+    }
+}
