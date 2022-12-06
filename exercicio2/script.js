978f1023f05791a62555dbfa21f286743779eed4
Perkles <gabriel190120@outlook.com>
2022-12-06 15:41:32 -0300

    correcao exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,9 @@
 let input = [prompt('Por favor, insira um número')]
-let indicesX = [0,1,2,3,4,5,6,7,8,9,10]
+let tabuada = [0,1,2,3,4,5,6,7,8,9,10]
 
 
 console.log(`Tabuada de ${input}`)
-for (const chave in indicesX) {
-    console.log(`${input} X ${indicesX[chave]} = ${input*indicesX[chave]}`)
+for (const indice in tabuada) {
+    console.log(`${input} X ${tabuada[indice]} = ${input*tabuada[indice]}`)
         
     };
