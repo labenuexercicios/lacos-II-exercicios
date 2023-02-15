9e2cfb6b4421320799913477516f05dcd731912f
Juliano Amaral <jaygovinda13@gmail.com>
2023-02-15 10:21:14 -0300

    revisão laços - II feita

---
+++ exercicio2/script.js
@@ -0,0 +1,5 @@
+const num = prompt(`Digite um numero`)
+
+for (let i = 0; i <= 10 ; i++) {
+    console.log(`${i} X ${num} = ${num*[i]}`)
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+for (let i = 0 ; i <= maioresPaises.length ; i++) {
+    console.log(`${Number(i +1)} - ${maioresPaises[i]}`)
+}
