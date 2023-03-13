decf9985309b258a7a481f697077f45e8a254a6b
LivanDoVale <livanjr4@gmail.com>
2023-03-13 11:03:39 -0300

    Tarefa 3 feita

---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+const bestAnimes = ["Jujutsu kaisen","Akame ga kill","Chainsaw man","Blue lock","Boku no hero"]
+
+for (let posicao = 0; posicao<bestAnimes.length; posicao+=1){
+    console.log(`${posicao+1} - ${bestAnimes[posicao]}`)    
+}
