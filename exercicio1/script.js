4d68b07c38602f5f2eefb7ea71296a74b33d4bd4
Wallacy <wallacy_vinicius@hotmail.com>
2023-06-12 11:56:36 -0300

    lacos-II-exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,15 @@
+// # Exercício 1
+
+// Uma pessoa analista quer conseguir ver quantos gols cada um de seus 5 atacantes marcou nas últimas 5 temporadas, e quer que você construa um código que mostre isso.
+
+// Crie um laco `for` que itere pelo array deixado no arquivo `script.js`, e em sequência itere pelo array que existe dentro do primeiro array. Você deve imprimir a temporada, e o número de gols, como no exemplo abaixo:
+
+// ```bash
+// Jogador 1: 13, 15, 25, 34, 35
+// Jogador 2: 30, 48, 30, 12, 47
+// ```
+
+
 const array = [
     [27, 4, 20, 13, 14],
     [11, 15, 12, 8, 9],
@@ -5,3 +17,9 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+let contador = 1;
+for (let i in array) {
+    console.log(`Jogador ${contador} Marcou: ${array[i]} Gols`)
+    contador++;
+  }
