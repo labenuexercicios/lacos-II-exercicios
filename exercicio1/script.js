9ef7a058f25fb952022f698f110e2404797b3302
Amanda <amanda.digital@hotmail.com>
2023-09-21 21:49:38 -0300

    finalizar exercicios lacos II

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,21 @@
-const array = [
+  /*  Uma pessoa analista quer conseguir ver quantos gols cada u
+    m de seus 5 atacantes marcou nas últimas 5 temporadas, e quer 
+    que você construa um código que mostre isso.
+
+    Crie um laco `for` que itere pelo array deixado no 
+    arquivo `script.js`, e em sequência itere pelo array que existe dentro do primeiro array. 
+    Você deve imprimir a temporada, e o número de gols, como no exemplo abaixo:*/
+
+
+const golsDosAtacantes = [
     [27, 4, 20, 13, 14],
     [11, 15, 12, 8, 9],
     [5, 5, 12, 16, 4],
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+for (let jogador = 0; jogador < golsDosAtacantes.length; jogador++) {
+    console.log(`Jogador ${jogador + 1}: ${golsDosAtacantes[jogador]}`);
+}
+        
---
+++ exercicio3/script.js
@@ -0,0 +1,27 @@
+/*Crie um array com 5 strings. Faça um programa que percorra 
+este array e imprima as strings em formato de ranking, como no exemplo abaixo:
+
+**Utilize o for...of para resolver**
+
+
+```jsx
+//entrada
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+
+//saída
+"1 - Rússia"
+"2 - Canadá"
+"3 - China"
+"4 - EUA"
+"5 - Brasil"
+```*/
+
+
+const maioresPaises = [ "Rússia", "Canadá", "China", "Eua", "Brasil"]
+ let ranking = 1
+
+  for (const pais of maioresPaises) { 
+     console.log (`${ranking } - ${pais}`); ranking ++
+
+     }
+
